35e37ddf10425262a32f1210c07c90381eb92a34
Matheus <matheustorresdev@gmail.com>
2023-12-18 20:33:21 -0300

    adicionando fontes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import { ThemeProvider } from 'styled-components/'
-import { Groups } from '@screens/Groups';
-import theme from '@theme/index';
+import { ActivityIndicator } from "react-native";
+import { ThemeProvider } from "styled-components/";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
+import { Groups } from "@screens/Groups";
+import theme from "@theme/index";
 
 export default function App() {
+  const [fonstLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-    <Groups />
+      {fonstLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
-
   );
 }
-
