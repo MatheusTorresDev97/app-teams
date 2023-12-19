e709fe47a6fd962fc9909b08743566706f1d32db
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-19 07:33:21 -0300

    criando o componente de highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,17 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
+import { Hightlight } from "@components/Highlight";
 
 
 export const Groups = () => {
   return (
     <Container>
       <Header />
+
+      <Hightlight 
+      title="Turmas"
+      subtitle="jogue com a sua turma"
+      />
     </Container>
   );
 };
