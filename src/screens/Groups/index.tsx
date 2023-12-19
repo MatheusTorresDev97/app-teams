5e96e4ddd289fc10b698f5842d5b697baa4d247b
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-19 08:18:44 -0300

    listagem de grupos com flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Hightlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 
-
 export const Groups = () => {
+  const [groups, setGroups] = useState<string[]>(["Galera do NTI", "Amigos do NTI"]);
+
   return (
     <Container>
       <Header />
 
-      <Hightlight 
-      title="Turmas"
-      subtitle="jogue com a sua turma"
+      <Hightlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} 
+
+          />
+        )}
       />
 
-      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 };
