db0a3b15a9db25758094cd20154833d2071973f7
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-19 08:28:22 -0300

    componente button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { TouchableOpacityProps } from "react-native";
+
+import { ButtonTypeStyleProps, Container, Title } from "./styles";
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  type?: ButtonTypeStyleProps;
+};
+
+export const Button = ({ title, type = "PRIMARY", ...props }: Props) => {
+  return (
+    <Container type={type} {...props}>
+      <Title>{title}</Title>
+    </Container>
+  );
+};
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Header } from "@components/Header";
-import { Container } from "./styles";
 import { Hightlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
+
+import { Container } from "./styles";
 
 export const Groups = () => {
-  const [groups, setGroups] = useState<string[]>(["Galera do NTI", "Amigos do NTI"]);
+  const [groups, setGroups] = useState<string[]>([
+    "Galera do NTI",
+    "Amigos do NTI",
+  ]);
 
   return (
     <Container>
@@ -18,17 +23,14 @@ export const Groups = () => {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <GroupCard title={item} 
-
-          />
-        )}
+        renderItem={({ item }) => <GroupCard title={item} />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
 
+      <Button title="Criar nova turma" />
     </Container>
   );
 };
