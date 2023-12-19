6d740c65bcf32b1edd5bf8cf95f613812a357782
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-19 09:06:10 -0300

    Dinamico Icon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,15 +1,21 @@
 import { TouchableOpacityProps } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'
 
-import { Container, Icon } from './styles';
+import { Container, Icon, ButtonIconTypeStyleProps } from './styles';
 
 type Props = TouchableOpacityProps & {
 
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: ButtonIconTypeStyleProps
 }
 
-export const ButtonIcon = ({}: Props) => {
+export const ButtonIcon = ({ icon, type = 'PRIMARY', ...props }: Props) => {
     return (
-        <Container>
-        <Icon name="home" type="PRIMARY" />
+        <Container {...props}>
+         <Icon 
+        name={icon}
+        type={type} 
+      />
       </Container>
     )
 }
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,8 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import { Hightlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
 
 export const Players = () => {
   return (
@@ -13,7 +14,16 @@ export const Players = () => {
         subtitle="adicione a galera e separe os times"
       />
 
-      <ButtonIcon />
+     <Form>
+     <Input 
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+        />
+
+        <ButtonIcon 
+          icon="add" 
+        />
+     </Form>
     </Container>
   );
 };
