690a2364f866d7426728b80453c1fb2d6c3c2e15
Matheus <matheustorresdev@gmail.com>
2023-12-18 21:01:59 -0300

    importando biblioteca de icones e criando btn de voltar

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from "@components/Header";
 import { Container } from "./styles";
 
 
 export const Groups = () => {
   return (
     <Container>
+      <Header />
     </Container>
   );
 };
