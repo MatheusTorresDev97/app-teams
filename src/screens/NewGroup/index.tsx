52570f1fede7d385e1b7146a62a9879350e66b90
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-12-20 08:12:50 -0300

    implementando o voltar

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Container, Content, Icon } from "./styles";
 
 import { Header } from "@components/Header";
@@ -6,6 +8,12 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 export const NewGroup = () => {
+  const navigation = useNavigation();
+
+  const handleNew = () => {
+    navigation.navigate("players", { group: "NTI" });
+  };
+
   return (
     <Container>
       <Header showBackButton />
@@ -18,11 +26,9 @@ export const NewGroup = () => {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input 
-        placeholder="Nome da turma"
-        />
+        <Input placeholder="Nome da turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
