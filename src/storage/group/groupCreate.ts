c6a665d4223378cb51fac6810fb7dbed6fda97b5
Matheus <matheustorresdev@gmail.com>
2023-12-20 20:42:17 -0300

    obtendo os grupos

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,11 +1,15 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { GROUP_COLLECTION } from '@storage/group/storageConfig';
+import { groupsGetAll } from "./groupsGetAll";
+import { GROUP_COLLECTION } from "@storage/storageConfig";
 
 export const groupCreate = async (newGroup: string) => {
-    try {
-        await AsyncStorage.setItem(GROUP_COLLECTION, newGroup);
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const storedGroups = await groupsGetAll();
+
+    const storage = JSON.stringify([...storedGroups, newGroup]);
+    await AsyncStorage.setItem(GROUP_COLLECTION, storage);
+  } catch (error) {
+    throw error;
+  }
+};
